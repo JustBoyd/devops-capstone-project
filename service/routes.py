f97efb51270e3772d1bbfd6e444c75c1849ff998
JustBoyd <78034940+JustBoyd@users.noreply.github.com>
2023-05-02 12:23:10 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -59,11 +59,12 @@ def create_accounts():
 
 ######################################################################
 # LIST ALL ACCOUNTS
+
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
-def list_accounts():
-    
-    
+def list_accounts(): 
     """
     List all Accounts
     This endpoint will list all Accounts
@@ -77,6 +78,7 @@ def list_accounts():
 
 ######################################################################
 # READ AN ACCOUNT
+
 ######################################################################
 
 
@@ -97,12 +99,12 @@ def get_accounts(account_id):
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
+
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-def update_accounts(account_id):
-    
-    
+def update_accounts(account_id):       
     """
     Update an Account
     This endpoint will update an Account based on the posted data
@@ -118,12 +120,12 @@ def update_accounts(account_id):
 
 ######################################################################
 # DELETE AN ACCOUNT
+
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_accounts(account_id):
-    
-    
+def delete_accounts(account_id):    
     """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
